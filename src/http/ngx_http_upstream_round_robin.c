d6921ad6194cbb5cf7a3521a7b9f7e645ebeb725
hanjian.lhj <hanjian.lhj@alibaba-inc.com>
2015-08-06 18:30:15 +0800

    upstream tries upstream peers new api

--- src/http/ngx_http_upstream_round_robin.c
+++ src/http/ngx_http_upstream_round_robin.c
@@ -483,11 +483,6 @@ ngx_http_upstream_get_round_robin_peer(ngx_peer_connection_t *pc, void *data)
     pc->socklen = peer->socklen;
     pc->name = &peer->name;
     pc->host = &peer->host;
-/*
-    if (pc->tries == 1 && rrp->peers->next) {
-        pc->tries += rrp->peers->next->number;
-    }
-*/
 
     /* ngx_unlock_mutex(peers->mutex); */
 
@@ -502,7 +497,6 @@ failed:
         ngx_log_debug0(NGX_LOG_DEBUG_HTTP, pc->log, 0, "backup servers");
 
         rrp->peers = peers->next;
-//        pc->tries = rrp->peers->number;
 
         n = (rrp->peers->number + (8 * sizeof(uintptr_t) - 1))
                 / (8 * sizeof(uintptr_t));
